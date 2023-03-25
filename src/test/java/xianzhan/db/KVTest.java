fc78a3660efad407298f39b441ee21938320f7db
xianzhan <424447832@qq.com>
2023-03-25 20:47:03 +0800

    config: pom 添加 lombok, slf4j2, log4j2

---
+++ src/test/java/xianzhan/db/KVTest.java
@@ -0,0 +1,17 @@
+package xianzhan.db;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author xianzhan
+ * @since 2023-03-25
+ */
+@Slf4j
+public class KVTest {
+
+    @Test
+    public void test() {
+        log.info("test");
+    }
+}
